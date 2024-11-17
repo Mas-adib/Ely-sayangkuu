02a33904bbe2597fcedd7d5f997ccc374ab95888
Mas-adib <adibalhakim6@gmail.com>
2024-11-17 20:14:39 +0700

    Add files via upload

---
+++ El.js
@@ -0,0 +1,18 @@
+function showLetter() {
+    const letter = `Dear Ely,
+    
+    Seng makasihhh ya udah jadi gadis maniss yang lahir didunia inii , tetep jadi dirimu yang baik ya maniss di era orang" pada pergaulan bebas, mas proudd sama kamu , makasih juga yaa udah selalu maafin mas kalo mas buat salahh, yang sabar yaa perjalanan kitaa masih puanjangg, yang semangatt jugaa yaa perjalanan karir sama pendidikan kitaa masih luas ,let's grow together sayangg
+    
+    Love<3, 
+    masAdib`;
+
+    document.getElementById('loveLetter').innerText = letter;
+}
+
+function checkAnswer(answer) {
+    if (answer === 'biru') {
+        alert('Bennerrr!,Aku suka biru tapi lebih suka kamuu.');
+    } else {
+        alert('Masa salahhh sih beb HAHAHA.');
+    }
+}
